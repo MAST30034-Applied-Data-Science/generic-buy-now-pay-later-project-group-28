f6f9349388ee317e18eee983dfebe68218c924a8
J1ARU1 <jiaruiq@student.unimelb.edu.au>
2022-09-18 22:09:16 +0930

    update on sa2 postcode linkage

--- scripts/unzip.py
+++ scripts/unzip.py
@@ -11,6 +11,10 @@ with ZipFile(raw_data_path+'sa2_2011_2016.zip', 'r') as zipObj:
    # Extract all the contents of zip file in current directory
    zipObj.extractall(raw_data_path)
 
-with ZipFile(raw_data_path+'poa_sa2.zip', 'r') as zipObj:
+with ZipFile(raw_data_path+'poa_sa2_2011.zip', 'r') as zipObj:
    # Extract all the contents of zip file in current directory
    zipObj.extractall(raw_data_path)
+
+with ZipFile(raw_data_path+'poa_sa2_2016.zip', 'r') as zipObj:
+   # Extract all the contents of zip file in current directory
+   zipObj.extract('CG_POA_2016_SA2_2016.xls',raw_data_path)
